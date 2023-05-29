7c0236d0e0a8b0e760201fbf4131926190f9b9cf
INGRYD SHIRLLEY DA SILVA RIBEIRO <22282092@CFP127ALUNO.LOCAL>
2023-05-29 11:28:00 -0300

    oi

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
+'use strict'
 
 export const produtos = async () => {
+   
     // let token = "d5e35518b330cab7beb621b5c68cad84";
-    const url = `http://localhost:8080/v1/tomorrows-water/produto`
+    const url = `http://localhost:8080/v1/tomorrows-water/patrocinador/2`
     const response = await fetch(url)
     const data = await response.json()
     const { produtos } = data;
+    console.log(produtos)
     return {
         ...data
     }
+    
+    
 }
--- carregarProdutos.js
+++ carregarProdutos.js
@@ -1,7 +1,8 @@
 'use strict'
 
-import { produtos } from "./app.JS"
+import { produtos } from "./app.js"
 let dados = await produtos();
+console.log(dados)
 
 const cardProduct = (dados) => {
 
@@ -10,31 +11,31 @@ card.classList.add('card')
 
 const name = document.createElement('h2')
 name.classList.add('card__name')
-name.textContent = dados.nome;
+name.textContent = dados.razao_social;
 
 const description = document.createElement('p')
 description.classList.add('card__biography')
-description.textContent = dados.descricao;
+description.textContent = dados.cnpj;
 
-const img = document.createElement('p')
-img.classList.add('card__img')
-img.textContent = dados.imagem;
+const email = document.createElement('p')
+email.classList.add('card__img')
+email.textContent = dados.email;
 
-const price = document.createElement('p')
-price.classList.add('card__price')
-price.textContent = dados.preco;
+// const price = document.createElement('p')
+// price.classList.add('card__price')
+// price.textContent = dados.preco;
 
-card.append(name, img, description, price)
+card.append(name, email, description)
 
 return card
 
 }
 
-export const carregarObjetos = async () => {
+export const carregarProduct = async () => {
 
 const container = document.getElementById('container-collections')
 const cards =  dados.map(cardProduct)
 container.append(...cards)
 }
 
-carregarObjetos()                                                                       
+carregarProduct()
